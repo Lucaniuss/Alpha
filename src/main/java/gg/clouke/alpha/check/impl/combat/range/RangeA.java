584651e10fcb9ab4de27f8771e10ba0edd302648
Clouke <76826179+Clouke@users.noreply.github.com>
2022-02-04 19:47:17 +0100

    finished reach a, not tested yet

--- src/main/java/gg/clouke/alpha/check/impl/combat/range/RangeA.java
+++ src/main/java/gg/clouke/alpha/check/impl/combat/range/RangeA.java
@@ -1,34 +1,60 @@
 package gg.clouke.alpha.check.impl.combat.range;
 
 import gg.clouke.alpha.check.Check;
-import gg.clouke.alpha.check.api.BaseCheck;
+import gg.clouke.alpha.check.BaseCheck;
 import gg.clouke.alpha.packet.Packet;
 import gg.clouke.alpha.profile.Profile;
-import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
-import io.github.retrooper.packetevents.event.impl.PacketPlaySendEvent;
-import io.github.retrooper.packetevents.packettype.PacketType;
-import io.github.retrooper.packetevents.packetwrappers.play.in.useentity.WrappedPacketInUseEntity;
-import org.bukkit.Location;
+import gg.clouke.alpha.tracker.PositionTracker;
+import gg.clouke.alpha.wrapper.PositionWrapper;
+import io.github.retrooper.packetevents.packetwrappers.play.in.flying.WrappedPacketInFlying;
+import org.bukkit.util.Vector;
 
-@BaseCheck(name = "Range", type = "A")
-public final class RangeA extends Check {
+/**
+ * @author Clouke
+ * @since 04.02.2022 18:48
+ * All Rights Reserved
+ *
+ * <p>
+ * Reach Class with a transaction detection of 3.009 ~ 3.03
+ * Using {@link PositionTracker} and wrapping into a Vector with {@link PositionWrapper}
+ */
+
+@BaseCheck(name = "Range", type = "A", experimental = true)
+public class RangeA extends Check {
 
     private boolean sent;
 
-    public RangeA(final Profile data) {
-        super(data);
+    public RangeA(final Profile profile) {
+        super(profile);
     }
 
     @Override
     public void handle(final Packet packet) {
         if (packet.isUseEntity()) {
-            if (isAttacking(packet)) {
+            if (isAttacking(packet))
                 sent = true;
-            }
         } else if (packet.isFlying()) {
-            if (!sent) return;
+            if (sent) {
 
-            sent = false;
+                final WrappedPacketInFlying wrapper = new WrappedPacketInFlying(packet.getRawPacket());
+
+                final PositionTracker tracker = profile.getPositionTracker();
+                final Vector player = new PositionWrapper(tracker.getX(), tracker.getY(), tracker.getZ()).toVector();
+                final Profile targetProfile = getTargetProfile();
+
+                final PositionTracker targetTracker = targetProfile.getPositionTracker();
+                final Vector target = new PositionWrapper(targetTracker.getX(), targetTracker.getY(), targetTracker.getZ()).toVector();
+
+                final double distance = this.getDistance(player, target);
+                final double threshold = wrapper.isPosition() ? 3.009 : 3.03;
+
+                debug("D: " + distance + " T: " + threshold);
+                if (distance >= threshold) {
+                    alert("D: " + distance + " T: " + threshold);
+                }
+
+                sent = false;
+            }
         }
     }
 }
