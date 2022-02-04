00f39cc366cd3b34b1a5d8fae0f827842150a274
Clouke <76826179+Clouke@users.noreply.github.com>
2022-02-04 19:44:52 +0100

    cleaned code

--- src/main/java/gg/clouke/alpha/Alpha.java
+++ src/main/java/gg/clouke/alpha/Alpha.java
@@ -1,34 +1,23 @@
 package gg.clouke.alpha;
 
 import gg.clouke.alpha.profile.ProfileRouter;
+import gg.clouke.alpha.util.command.CommandFramework;
 import io.github.retrooper.packetevents.PacketEvents;
 import io.github.retrooper.packetevents.utils.server.ServerVersion;
 import lombok.Getter;
-import gg.clouke.alpha.check.manager.CheckManager;
-import gg.clouke.alpha.listener.bukkit.RegistrationListener;
-import gg.clouke.alpha.listener.packet.PacketListener;
+import gg.clouke.alpha.check.registry.CheckRegistry;
+import gg.clouke.alpha.profile.ProfileRegistry;
+import gg.clouke.alpha.network.NetworkProvider;
 import org.bukkit.Bukkit;
 
 @Getter
 public enum Alpha {
 
-    /**
-     * This is our enum instance for our AntiCheat class. You can use this to access non-static stuff in this class.
-     * You may ask why enum instance and that is a great question, the answer is its cooler and cleaner than the old fashion way.
-     */
     INSTANCE;
 
-    /**
-     * This is where we store our main plugin instance.
-     * If we need to use it we can easily access it here.
-     */
     private AlphaPlugin plugin;
-
-    /**
-     * This is where all our PlayerData is managed.
-     * This gets cleared on onEnable and on onDisable.
-     */
-    private ProfileRouter playerDataManager;
+    private ProfileRouter profileRouter;
+    private CommandFramework commandFramework;
 
     /**
      * This method gets called on load.
@@ -49,37 +38,22 @@ public enum Alpha {
      * @param plugin Main plugin instance.
      */
     public void init(final AlphaPlugin plugin) {
-        playerDataManager = new ProfileRouter();
-        Bukkit.getOnlinePlayers().forEach(player -> playerDataManager.add(player));
-
-        CheckManager.setup();
-
+        profileRouter = new ProfileRouter();
+        Bukkit.getOnlinePlayers().forEach(player -> profileRouter.add(player));
+        CheckRegistry.setup();
         startPacketEvents(plugin);
         handleBukkit(plugin);
+        commandFramework = new CommandFramework(plugin);
     }
 
-    /**
-     * This method gets called on plugin enable.
-     * We can disable our plugin and cancel all tasks here.
-     *
-     * @param plugin Main plugin instance.
-     */
     public void stop(final AlphaPlugin plugin) {
         stopPacketEvents(plugin);
         Bukkit.getScheduler().cancelTasks(plugin);
 
-        playerDataManager = null;
+        profileRouter = null;
     }
 
-    /*
-    Functional programming good so we do cool stuff.
-     */
 
-    /**
-     * We create and load PacketEvents.
-     *
-     * @param plugin Main plugin instance.
-     */
     private void loadPacketEvents(final AlphaPlugin plugin) {
         PacketEvents.create(plugin).getSettings()
                 .checkForUpdates(false)
@@ -88,35 +62,19 @@ public enum Alpha {
         PacketEvents.get().load();
     }
 
-    /**
-     * Start PacketEvents and register our packet listener.
-     *
-     * @param plugin Main plugin instance.
-     */
     private void startPacketEvents(final AlphaPlugin plugin) {
         PacketEvents.get().init();
 
-        PacketEvents.get().getEventManager().registerListener(new PacketListener());
+        PacketEvents.get().getEventManager().registerListener(new NetworkProvider());
     }
 
-    /**
-     * Stop PacketEvents.
-     *
-     * @param plugin Main plugin instance.
-     */
     private void stopPacketEvents(final AlphaPlugin plugin) {
         PacketEvents.get().terminate();
     }
 
-    /**
-     * This method will handle everything related to bukkit on enable.
-     *
-     * @param plugin Main plugin instance.
-     */
     private void handleBukkit(final AlphaPlugin plugin) {
         plugin.saveDefaultConfig();
 
-        plugin.getServer().getPluginManager().registerEvents(new RegistrationListener(), plugin);
-        plugin.getServer().getPluginManager().registerEvents(new BukkitEventListener(), plugin);
+        plugin.getServer().getPluginManager().registerEvents(new ProfileRegistry(), plugin);
     }
 }
