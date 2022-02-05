60c04378b52e76a652e857ca2210d2a84b231629
Clouke <76826179+Clouke@users.noreply.github.com>
2022-02-05 02:44:54 +0100

    new utils

---
+++ src/main/java/gg/clouke/alpha/util/Pair.java
@@ -0,0 +1,14 @@
+package gg.clouke.alpha.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public final class Pair<X, Y> {
+
+    private X x;
+    private Y y;
+
+}
+
