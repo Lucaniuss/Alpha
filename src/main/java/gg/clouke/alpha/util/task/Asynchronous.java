e1ab5b70b526a621106ba8183c11ed3c3b166e86
Clouke <76826179+Clouke@users.noreply.github.com>
2022-05-29 20:48:20 +0200

    changed to new plugin instance

--- src/main/java/gg/clouke/alpha/util/task/Asynchronous.java
+++ src/main/java/gg/clouke/alpha/util/task/Asynchronous.java
@@ -1,7 +1,6 @@
 package gg.clouke.alpha.util.task;
 
 import gg.clouke.alpha.Alpha;
-import gg.clouke.alpha.AlphaPlugin;
 
 /**
  * @author Clouke
@@ -10,7 +9,7 @@ import gg.clouke.alpha.AlphaPlugin;
  */
 public class Asynchronous {
 
-    private static final AlphaPlugin plugin = Alpha.INSTANCE.getPlugin();
+    private static final Alpha plugin = Alpha.getInstance();
 
     public static void run(Runnable runnable) {
         plugin.getServer().getScheduler().runTaskAsynchronously(plugin, runnable);
