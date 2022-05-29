3e9291d7e4eac92e7c0568c04498a1358f450503
Clouke <76826179+Clouke@users.noreply.github.com>
2022-05-29 21:03:05 +0200

    Alpha `2.0`

--- src/main/java/gg/clouke/alpha/Alpha.java
+++ src/main/java/gg/clouke/alpha/Alpha.java
@@ -1,84 +1,70 @@
 package gg.clouke.alpha;
 
-import gg.clouke.alpha.module.alert.AlertModule;
-import gg.clouke.alpha.profile.ProfileRouter;
-import gg.clouke.alpha.tracker.TickTracker;
-import gg.clouke.alpha.util.clazz.ClassRegistration;
+import gg.clouke.alpha.check.CheckFactory;
+import gg.clouke.alpha.profile.ProfileProvider;
+import gg.clouke.alpha.profile.container.standard.ContainerFactory;
+import gg.clouke.alpha.profile.impl.BukkitProfileInjector;
+import gg.clouke.alpha.profile.impl.StandardProfileProvider;
+import gg.clouke.alpha.provider.alert.AlertProvider;
+import gg.clouke.alpha.provider.alert.impl.StandardAlertProvider;
+import gg.clouke.alpha.provider.event.EventBus;
+import gg.clouke.alpha.provider.event.EventProvider;
+import gg.clouke.alpha.provider.network.NetworkProvider;
+import gg.clouke.alpha.provider.network.impl.PacketProvider;
+import gg.clouke.alpha.provider.network.impl.StandardNetworkProvider;
+import gg.clouke.alpha.provider.thread.impl.ThreadWatcher;
+import gg.clouke.alpha.provider.tick.TickProvider;
+import gg.clouke.alpha.provider.tick.impl.TickProviderFactory;
+import gg.clouke.alpha.util.clazz.ClassService;
 import gg.clouke.alpha.util.command.CommandFramework;
 import gg.clouke.alpha.util.config.ConfigFile;
-import io.github.retrooper.packetevents.PacketEvents;
-import io.github.retrooper.packetevents.utils.server.ServerVersion;
 import lombok.Getter;
-import gg.clouke.alpha.check.registry.CheckRegistry;
-import gg.clouke.alpha.profile.ProfileRegistry;
-import gg.clouke.alpha.network.NetworkProvider;
-import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter
-public enum Alpha {
+public final class Alpha extends JavaPlugin {
 
-    INSTANCE;
+    @Getter
+    private static Alpha instance;
 
-    private AlphaPlugin plugin;
-    private ProfileRouter profileRouter;
+    private PacketProvider packetProvider;
+    private AlertProvider alertProvider;
+    private CheckFactory checkFactory;
+    private ContainerFactory containerFactory;
+    private EventProvider eventProvider;
+    private TickProvider tickProvider;
+    private ProfileProvider profileProvider;
     private CommandFramework commandFramework;
-    private TickTracker tickTracker;
-    private AlertModule alertModule;
+    private NetworkProvider networkProvider;
+    private ThreadWatcher threadWatcher;
     private ConfigFile config;
     private boolean debug;
 
-    public void load(final AlphaPlugin plugin) {
-        this.plugin = plugin;
+    @Override
+    public void onEnable() {
+        instance = this;
 
-        PacketEvents.create(plugin).getSettings()
-                .checkForUpdates(false)
-                .backupServerVersion(ServerVersion.v_1_8_8);
+        this.config = new ConfigFile(this, "config.yml");
+        this.containerFactory = new ContainerFactory();
+        this.checkFactory = new CheckFactory();
+        this.commandFramework = new CommandFramework(this);
+        this.packetProvider = new PacketProvider();
+        this.networkProvider = new StandardNetworkProvider();
+        this.profileProvider = new StandardProfileProvider();
+        this.alertProvider = new StandardAlertProvider();
+        this.threadWatcher = new ThreadWatcher();
+        this.tickProvider = new TickProviderFactory();
+        this.eventProvider = new EventBus();
 
-        PacketEvents.get().load();
+        ClassService.loadCommandsFromPackage(this, "gg.clouke.alpha.command");
+        new BukkitProfileInjector();
+
+        this.tickProvider.start();
     }
 
-    public void init(final AlphaPlugin plugin) {
-        registerConfig();
-        profileRouter = new ProfileRouter();
-        startPacketEvents();
-        handleBukkit(plugin);
-        registerCommands();
-        registerModules();
+    @Override
+    public void onDisable() {
+        this.tickProvider.stop();
     }
 
-    private void registerConfig() {
-        this.config = new ConfigFile(plugin, "config.yml");
-    }
-
-    private void registerModules() {
-        CheckRegistry.setup();
-        (tickTracker = new TickTracker()).runTaskTimerAsynchronously(plugin, 0L, 1L);
-        alertModule = new AlertModule();
-        this.debug = config.getBoolean("settings.debug");
-    }
-
-    private void registerCommands() {
-        commandFramework = new CommandFramework(plugin);
-        ClassRegistration.loadCommandsFromPackage(plugin, "gg.clouke.alpha.command");
-    }
-
-    public void stop(final AlphaPlugin plugin) {
-        stopPacketEvents();
-        Bukkit.getScheduler().cancelTasks(plugin);
-        this.config.save();
-        this.profileRouter = null;
-    }
-
-    private void startPacketEvents() {
-        PacketEvents.get().init();
-        PacketEvents.get().getEventManager().registerListener(new NetworkProvider());
-    }
-
-    private void stopPacketEvents() {
-        PacketEvents.get().terminate();
-    }
-
-    private void handleBukkit(final AlphaPlugin plugin) {
-        plugin.getServer().getPluginManager().registerEvents(new ProfileRegistry(), plugin);
-    }
 }
