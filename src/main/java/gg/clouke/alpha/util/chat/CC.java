85661dae5b7e0a6e984f1e2aa6a4e9dd48f8581f
Lucanius69 <lucaisdik7@gmail.com>
2022-05-31 21:40:12 +0200

    Added `LINE` to `CC`

--- src/main/java/gg/clouke/alpha/util/chat/CC.java
+++ src/main/java/gg/clouke/alpha/util/chat/CC.java
@@ -5,6 +5,9 @@ import org.bukkit.ChatColor;
 
 @UtilityClass
 public final class CC {
+
+    public final String LINE = "&7&m---------------------------------";
+
     public String translate(final String string) {
         return ChatColor.translateAlternateColorCodes('&', string);
     }
