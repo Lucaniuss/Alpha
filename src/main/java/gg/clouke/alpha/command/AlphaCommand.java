d00db64bf294f5cc645a764502ca0d09efa1199c
Lucanius69 <lucaisdik7@gmail.com>
2022-05-31 21:20:40 +0200

    Added Completer on `AlphaCommand` and optimized it

--- src/main/java/gg/clouke/alpha/command/AlphaCommand.java
+++ src/main/java/gg/clouke/alpha/command/AlphaCommand.java
@@ -5,11 +5,11 @@ import gg.clouke.alpha.util.clazz.ClassService;
 import gg.clouke.alpha.util.command.BaseCommand;
 import gg.clouke.alpha.util.command.Command;
 import gg.clouke.alpha.util.command.CommandArgs;
-import gg.clouke.alpha.util.task.Asynchronous;
+import gg.clouke.alpha.util.command.Completer;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Clouke
@@ -18,20 +18,25 @@ import java.util.List;
  */
 public class AlphaCommand extends BaseCommand {
 
-    private static final List<String> commands = new ArrayList<>();
+    private final List<String> commands;
+
+    public AlphaCommand() {
+        commands = ClassService.getClasses(plugin, "gg.clouke.alpha.command.impl")
+                .stream()
+                .map(clazz -> clazz.getSimpleName().replace("Command", "").toLowerCase())
+                .collect(Collectors.toList());
+    }
 
     @Command(name = "alpha", permission = "alpha.command.alpha")
     public void onDispatch(final CommandArgs cmd) {
-        Asynchronous.run(() -> {
-            final Player player = cmd.getPlayer();
+        Player player = cmd.getPlayer();
 
-            if (commands.isEmpty()) { // Only initialize once
-                ClassService.getClasses(plugin, "gg.clouke.alpha.command.impl")
-                        .forEach(clazz -> commands.add(clazz.getSimpleName().toLowerCase().replace("command", "")));
-            }
+        player.sendMessage(CC.translate("&e&lAlpha Commands (&a&l" + commands.size() + "&e&l):"));
+        commands.forEach(command -> player.sendMessage(CC.translate("&7- &e/alpha &7" + command)));
+    }
 
-            player.sendMessage(CC.translate("&e&lAlpha Commands (&a&l" + commands.size() + "&e&l):"));
-            commands.forEach(command -> player.sendMessage(CC.translate("&8&l- &e/alpha &7" + command)));
-        });
+    @Completer(name = "alpha")
+    public List<String> onCompleter(CommandArgs cmd) {
+        return commands;
     }
 }
